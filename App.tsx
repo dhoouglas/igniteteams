887c62b4cc946831c390dfef56e18d4f8a42b4b9
dhoouglas <dhoouglas@hotmail.com>
2024-11-26 12:35:19 -0300

    chore: add theme and typing, font-roboto

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
 import { Groups } from "@screens/Groups/index";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
-  return <Groups />;
+  const [fontIsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontIsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+  );
 }
