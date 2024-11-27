51ebf95abb9a72db64791b0bb0dc520b86085554
dhoouglas <dhoouglas@hotmail.com>
2024-11-27 16:52:21 -0300

    chroe: add library icons, components for Groups, config FlatList and Button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { Groups } from "@screens/Groups/index";
-import { ThemeProvider } from "styled-components";
-import theme from "./src/theme";
-
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
+
+import theme from "./src/theme";
+
+import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  const [fontIsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontIsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
