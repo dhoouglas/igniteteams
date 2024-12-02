2d4bbbceceac689e57f5c2497f85dacc8a79ba40
dhoouglas <dhoouglas@hotmail.com>
2024-12-02 15:40:51 -0300

    chore: add react-navigation and config routes

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+import { Container, Content, Icon } from "./styles";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-import { Container, Content, Icon } from "./styles";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,9 +28,13 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da turma" />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
 
-        <Button title="Criar" style={{ marginTop: 20 }}></Button>
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handleNew}
+        ></Button>
       </Content>
     </Container>
   );
